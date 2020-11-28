b3abe7e0adc813c6e298b7ef030a90258653b3a0
Pentaonix <ducquang0301@gmail.com>
2020-11-28 09:15:44 +0000

    user authen for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   require "net/http"
-  before_action :authenticate, except: [:createorder, :get_invoice, :my_orders, :show]
+  before_action :authenticate, except: [:get_invoice, :my_orders, :show]
 
   def index
     @page = params.fetch(:page, 0).to_i || 0
@@ -25,21 +25,21 @@ class OrdersController < ApplicationController
   end
 
   def createorder
-    if session[:cart]
-      order = Order.new({ :user_id => current_user.id, :total => 0, :status => "Pending" })
-      tot = 0
-      if order.save
-        session[:cart].each do |order_item|
-          parameter = { :order_id => order.id, :menu_item_id => order_item["dish_id"], :quantity => order_item["quantity"], :total => get_total(order_item) }
-          tot = tot + get_total(order_item)
-          OrderItem.create!(parameter)
+      if session[:cart]
+        order = Order.new({ :user_id => current_user.id, :total => 0, :status => "Pending" })
+        tot = 0
+        if order.save
+          session[:cart].each do |order_item|
+            parameter = { :order_id => order.id, :menu_item_id => order_item["dish_id"], :quantity => order_item["quantity"], :total => get_total(order_item) }
+            tot = tot + get_total(order_item)
+            OrderItem.create!(parameter)
+          end
         end
+        session[:cart] = nil
+        order.total = tot
+        order.save
       end
-      session[:cart] = nil
-      order.total = tot
-      order.save
-    end
-    redirect_to dishes_path
+      redirect_to dishes_path
   end
 
   def mark_as_complete
